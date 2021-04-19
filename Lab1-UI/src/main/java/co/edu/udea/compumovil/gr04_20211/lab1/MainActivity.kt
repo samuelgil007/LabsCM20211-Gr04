4cb9aaa92b4aa48a78a8bb9c2943026406a28ab3
samuelgil007 <sagilar15@gmail.com>
2021-04-19 18:08:02 -0500

    ContactDataActivty sin lenguaje y sin el punto de logcat

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20211/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20211/lab1/MainActivity.kt
@@ -1,19 +1,22 @@
 package co.edu.udea.compumovil.gr04_20211.lab1
 
-import android.app.DatePickerDialog
 import android.content.Intent
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.DatePicker
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        bienvenida.setOnClickListener{
-            val intent: Intent = Intent(this, PersonalDataActivity::class.java)
-            startActivity(intent)
+        setContentView(R.layout.activity_main);
+    }
+
+    fun onClickContactButton(view: View) {
+        val intent = Intent(this, ContactDataActivty::class.java).apply {
         }
+        startActivity(intent)
     }
 }
