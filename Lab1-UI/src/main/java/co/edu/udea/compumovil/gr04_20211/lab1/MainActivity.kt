38caea0d2fb62deba507d7990c9858f3bbd058ad
Praridz <rios.371@hotmail.com>
2021-04-19 16:33:25 -0500

    Activity info personal added

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20211/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20211/lab1/MainActivity.kt
@@ -1,11 +1,19 @@
 package co.edu.udea.compumovil.gr04_20211.lab1
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bienvenida.setOnClickListener{
+            val intent: Intent = Intent(this, PersonalDataActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
